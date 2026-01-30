ef3ec18a7543bdc43f4bc187a9326180ce016f5d
Clownacy <Clownacy@users.noreply.github.com>
2026-01-30 15:04:24 +0000

    Merge `ClownCD_Disc` and `ClownCD`.

--- clowncd.c
+++ clowncd.c
@@ -10,95 +10,95 @@
 
 #define CLOWNCD_CRC_POLYNOMIAL 0xD8018001
 
-static cc_bool ClownCD_IsFrameValid(ClownCD* const state)
+static cc_bool ClownCD_IsFrameValid(ClownCD* const disc)
 {
-	return !(state->disc.track.current_frame < state->disc.track.starting_frame || state->disc.track.current_frame >= state->disc.track.starting_frame + state->disc.track.total_frames);
+	return !(disc->track.current_frame < disc->track.starting_frame || disc->track.current_frame >= disc->track.starting_frame + disc->track.total_frames);
 }
 
-static cc_bool ClownCD_SeekFrameInternal(ClownCD* const state, const size_t frame)
+static cc_bool ClownCD_SeekFrameInternal(ClownCD* const disc, const size_t frame)
 {
-	const size_t header_size = state->disc.track.header_size;
-	const size_t sector_size = state->disc.track.has_full_sized_sectors ? CLOWNCD_SECTOR_RAW_SIZE : CLOWNCD_SECTOR_DATA_SIZE;
+	const size_t header_size = disc->track.header_size;
+	const size_t sector_size = disc->track.has_full_sized_sectors ? CLOWNCD_SECTOR_RAW_SIZE : CLOWNCD_SECTOR_DATA_SIZE;
 
 	if (header_size == (size_t)-1)
 		return cc_false;
 
-	state->disc.track.current_frame = state->disc.track.starting_frame + frame;
+	disc->track.current_frame = disc->track.starting_frame + frame;
 
-	if (!ClownCD_IsFrameValid(state))
+	if (!ClownCD_IsFrameValid(disc))
 		return cc_false;
 
-	if (ClownCD_FileSeek(&state->disc.track.file, header_size + state->disc.track.current_frame / CLOWNCD_AUDIO_FRAMES_PER_SECTOR * sector_size + state->disc.track.current_frame % CLOWNCD_AUDIO_FRAMES_PER_SECTOR * CLOWNCD_AUDIO_FRAME_SIZE, CLOWNCD_SEEK_SET) != 0)
+	if (ClownCD_FileSeek(&disc->track.file, header_size + disc->track.current_frame / CLOWNCD_AUDIO_FRAMES_PER_SECTOR * sector_size + disc->track.current_frame % CLOWNCD_AUDIO_FRAMES_PER_SECTOR * CLOWNCD_AUDIO_FRAME_SIZE, CLOWNCD_SEEK_SET) != 0)
 		return cc_false;
 
 	return cc_true;
 }
 
-void ClownCD_OpenAlreadyOpen(ClownCD *state, void *stream, const char *file_path, const ClownCD_FileCallbacks *callbacks)
+void ClownCD_OpenAlreadyOpen(ClownCD *disc, void *stream, const char *file_path, const ClownCD_FileCallbacks *callbacks)
 {
-	state->disc.filename = ClownCD_DuplicateString(file_path); /* It's okay for this to fail. */
-	state->disc.file = stream != NULL ? ClownCD_FileOpenAlreadyOpen(stream, callbacks) : ClownCD_FileOpen(file_path, CLOWNCD_RB, callbacks);
+	disc->filename = ClownCD_DuplicateString(file_path); /* It's okay for this to fail. */
+	disc->file = stream != NULL ? ClownCD_FileOpenAlreadyOpen(stream, callbacks) : ClownCD_FileOpen(file_path, CLOWNCD_RB, callbacks);
 
-	state->disc.track.header_size = 0;
-	state->disc.track.audio_decoder_needed = cc_false;
-	state->disc.track.has_full_sized_sectors = cc_false;
-	state->disc.track.starting_frame = 0;
-	state->disc.track.current_frame = 0;
-	state->disc.track.total_frames = 0;
+	disc->track.header_size = 0;
+	disc->track.audio_decoder_needed = cc_false;
+	disc->track.has_full_sized_sectors = cc_false;
+	disc->track.starting_frame = 0;
+	disc->track.current_frame = 0;
+	disc->track.total_frames = 0;
 
-	ClownCD_DiscOpen(&state->disc);
+	ClownCD_DiscOpen(disc);
 }
 
-void ClownCD_Close(ClownCD* const state)
+void ClownCD_Close(ClownCD* const disc)
 {
-	if (ClownCD_FileIsOpen(&state->disc.track.file))
+	if (ClownCD_FileIsOpen(&disc->track.file))
 	{
-		if (state->disc.track.audio_decoder_needed)
-			ClownCD_AudioClose(&state->disc.track.audio);
+		if (disc->track.audio_decoder_needed)
+			ClownCD_AudioClose(&disc->track.audio);
 
-		ClownCD_FileClose(&state->disc.track.file);
+		ClownCD_FileClose(&disc->track.file);
 	}
 
-	if (ClownCD_FileIsOpen(&state->disc.file))
-		ClownCD_FileClose(&state->disc.file);
+	if (ClownCD_FileIsOpen(&disc->file))
+		ClownCD_FileClose(&disc->file);
 
-	free(state->disc.filename);
+	free(disc->filename);
 }
 
-static cc_bool ClownCD_SeekTrackIndexInternal(ClownCD* const state, const unsigned int track, const unsigned int index)
+static cc_bool ClownCD_SeekTrackIndexInternal(ClownCD* const disc, const unsigned int track, const unsigned int index)
 {
-	if (track != state->disc.track.current_track || index != state->disc.track.current_index)
+	if (track != disc->track.current_track || index != disc->track.current_index)
 	{
-		state->disc.track.current_track = track;
-		state->disc.track.current_index = index;
+		disc->track.current_track = track;
+		disc->track.current_index = index;
 
-		if (!ClownCD_DiscSeekTrackIndex(&state->disc, track, index))
+		if (!ClownCD_DiscSeekTrackIndex(disc, track, index))
 			return cc_false;
 
 		/* Force the frame to update. */
-		state->disc.track.current_frame = -1;
+		disc->track.current_frame = -1;
 	}
 
 	return cc_true;
 }
 
-cc_bool ClownCD_SeekAudioFrame(ClownCD* const state, const size_t frame)
+cc_bool ClownCD_SeekAudioFrame(ClownCD* const disc, const size_t frame)
 {
-	if (frame >= state->disc.track.total_frames)
+	if (frame >= disc->track.total_frames)
 		return cc_false;
 
-	if (frame != state->disc.track.current_frame)
+	if (frame != disc->track.current_frame)
 	{
-		state->disc.track.current_frame = frame;
+		disc->track.current_frame = frame;
 
-		if (state->disc.track.audio_decoder_needed)
+		if (disc->track.audio_decoder_needed)
 		{
-			if (!ClownCD_AudioSeek(&state->disc.track.audio, state->disc.track.starting_frame + frame))
+			if (!ClownCD_AudioSeek(&disc->track.audio, disc->track.starting_frame + frame))
 				return cc_false;
 		}
 		else
 		{
-			if (!ClownCD_SeekFrameInternal(state, state->disc.track.current_frame))
+			if (!ClownCD_SeekFrameInternal(disc, disc->track.current_frame))
 				return cc_false;
 		}
 	}
@@ -106,96 +106,96 @@ cc_bool ClownCD_SeekAudioFrame(ClownCD* const state, const size_t frame)
 	return cc_true;
 }
 
-cc_bool ClownCD_SetState(ClownCD* const state, const unsigned int track, const unsigned int index, const size_t frame)
+cc_bool ClownCD_SetState(ClownCD* const disc, const unsigned int track, const unsigned int index, const size_t frame)
 {
-	if (!ClownCD_SeekTrackIndexInternal(state, track, index))
+	if (!ClownCD_SeekTrackIndexInternal(disc, track, index))
 		return cc_false;
 
-	if (!ClownCD_SeekAudioFrame(state, frame))
+	if (!ClownCD_SeekAudioFrame(disc, frame))
 		return cc_false;
 
 	return cc_true;
 }
 
-cc_bool ClownCD_BeginSectorStream(ClownCD* const state)
+cc_bool ClownCD_BeginSectorStream(ClownCD* const disc)
 {
-	if (!ClownCD_IsFrameValid(state))
+	if (!ClownCD_IsFrameValid(disc))
 		return cc_false;
 
-	state->disc.track.current_frame += CLOWNCD_AUDIO_FRAMES_PER_SECTOR;
+	disc->track.current_frame += CLOWNCD_AUDIO_FRAMES_PER_SECTOR;
 
 	/* Skip header. */
-	if (state->disc.track.has_full_sized_sectors)
-		if (ClownCD_FileSeek(&state->disc.track.file, CLOWNCD_SECTOR_HEADER_SIZE, CLOWNCD_SEEK_CUR) != 0)
+	if (disc->track.has_full_sized_sectors)
+		if (ClownCD_FileSeek(&disc->track.file, CLOWNCD_SECTOR_HEADER_SIZE, CLOWNCD_SEEK_CUR) != 0)
 			return cc_false;
 
 	return cc_true;
 }
 
-size_t ClownCD_ReadSectorStream(ClownCD* const state, unsigned char* const buffer, const size_t total_bytes)
+size_t ClownCD_ReadSectorStream(ClownCD* const disc, unsigned char* const buffer, const size_t total_bytes)
 {
-	return ClownCD_FileRead(buffer, 1, total_bytes, &state->disc.track.file);
+	return ClownCD_FileRead(buffer, 1, total_bytes, &disc->track.file);
 }
 
-cc_bool ClownCD_EndSectorStream(ClownCD* const state)
+cc_bool ClownCD_EndSectorStream(ClownCD* const disc)
 {
 	/* Skip error-correction data. */
-	if (state->disc.track.has_full_sized_sectors)
-		if (ClownCD_FileSeek(&state->disc.track.file, CLOWNCD_SECTOR_RAW_SIZE - (CLOWNCD_SECTOR_HEADER_SIZE + CLOWNCD_SECTOR_DATA_SIZE), CLOWNCD_SEEK_CUR) != 0)
+	if (disc->track.has_full_sized_sectors)
+		if (ClownCD_FileSeek(&disc->track.file, CLOWNCD_SECTOR_RAW_SIZE - (CLOWNCD_SECTOR_HEADER_SIZE + CLOWNCD_SECTOR_DATA_SIZE), CLOWNCD_SEEK_CUR) != 0)
 			return cc_false;
 
 	return cc_true;
 }
 
-size_t ClownCD_ReadSector(ClownCD* const state, unsigned char* const buffer)
+size_t ClownCD_ReadSector(ClownCD* const disc, unsigned char* const buffer)
 {
 	size_t bytes_read;
 
-	if (!ClownCD_BeginSectorStream(state))
+	if (!ClownCD_BeginSectorStream(disc))
 		return 0;
 
-	bytes_read = ClownCD_ReadSectorStream(state, buffer, CLOWNCD_SECTOR_DATA_SIZE);
+	bytes_read = ClownCD_ReadSectorStream(disc, buffer, CLOWNCD_SECTOR_DATA_SIZE);
 
-	ClownCD_EndSectorStream(state);
+	ClownCD_EndSectorStream(disc);
 
 	return bytes_read;
 }
 
-static size_t ClownCD_ReadFramesGetAudio(ClownCD* const state, short* const buffer, const size_t total_frames)
+static size_t ClownCD_ReadFramesGetAudio(ClownCD* const disc, short* const buffer, const size_t total_frames)
 {
-	const size_t frames_to_do = CC_MIN(state->disc.track.total_frames - state->disc.track.current_frame, total_frames);
+	const size_t frames_to_do = CC_MIN(disc->track.total_frames - disc->track.current_frame, total_frames);
 
 	size_t frames_done;
 
-	if (state->disc.track.audio_decoder_needed)
+	if (disc->track.audio_decoder_needed)
 	{
-		frames_done = ClownCD_AudioRead(&state->disc.track.audio, buffer, frames_to_do);
+		frames_done = ClownCD_AudioRead(&disc->track.audio, buffer, frames_to_do);
 	}
 	else
 	{
 		short *buffer_pointer = buffer;
 
-		if (!state->disc.track.has_full_sized_sectors)
+		if (!disc->track.has_full_sized_sectors)
 			return 0;
 
 		for (frames_done = 0; frames_done < frames_to_do; ++frames_done)
 		{
-			*buffer_pointer++ = ClownCD_ReadS16LE(&state->disc.track.file);
-			*buffer_pointer++ = ClownCD_ReadS16LE(&state->disc.track.file);
+			*buffer_pointer++ = ClownCD_ReadS16LE(&disc->track.file);
+			*buffer_pointer++ = ClownCD_ReadS16LE(&disc->track.file);
 
-			if (state->disc.track.file.eof)
+			if (disc->track.file.eof)
 				break;
 		}
 	}
 
-	state->disc.track.current_frame += frames_done;
+	disc->track.current_frame += frames_done;
 
 	return frames_done;
 }
 
-static size_t ClownCD_ReadFramesGeneratePadding(ClownCD* const state, short* const buffer, const size_t total_frames)
+static size_t ClownCD_ReadFramesGeneratePadding(ClownCD* const disc, short* const buffer, const size_t total_frames)
 {
-	const size_t occupied_frames_in_sector = state->disc.track.current_frame % CLOWNCD_AUDIO_FRAMES_PER_SECTOR;
+	const size_t occupied_frames_in_sector = disc->track.current_frame % CLOWNCD_AUDIO_FRAMES_PER_SECTOR;
 	const size_t empty_frames_in_sector = occupied_frames_in_sector == 0 ? 0 : CLOWNCD_AUDIO_FRAMES_PER_SECTOR - occupied_frames_in_sector;
 
 	const size_t frames_to_do = CC_MIN(empty_frames_in_sector, total_frames);
@@ -205,10 +205,10 @@ static size_t ClownCD_ReadFramesGeneratePadding(ClownCD* const state, short* con
 	return frames_to_do;
 }
 
-size_t ClownCD_ReadFrames(ClownCD* const state, short* const buffer, const size_t total_frames)
+size_t ClownCD_ReadFrames(ClownCD* const disc, short* const buffer, const size_t total_frames)
 {
-	const size_t audio_frames_done = ClownCD_ReadFramesGetAudio(state, buffer, total_frames);
-	const size_t padding_frames_done = ClownCD_ReadFramesGeneratePadding(state, buffer + audio_frames_done * CLOWNCD_AUDIO_CHANNELS, total_frames - audio_frames_done);
+	const size_t audio_frames_done = ClownCD_ReadFramesGetAudio(disc, buffer, total_frames);
+	const size_t padding_frames_done = ClownCD_ReadFramesGeneratePadding(disc, buffer + audio_frames_done * CLOWNCD_AUDIO_CHANNELS, total_frames - audio_frames_done);
 
 	return audio_frames_done + padding_frames_done;
 }
--- clowncd.h
+++ clowncd.h
@@ -12,32 +12,29 @@
 #include "error.h"
 #include "file-io.h"
 
-typedef struct ClownCD
-{
-	ClownCD_Disc disc;
-} ClownCD;
+typedef ClownCD_Disc ClownCD;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define ClownCD_Open(state, file_path, callbacks) ClownCD_OpenAlreadyOpen(state, NULL, file_path, callbacks)
-void ClownCD_OpenAlreadyOpen(ClownCD *state, void *stream, const char *file_path, const ClownCD_FileCallbacks *callbacks);
-void ClownCD_Close(ClownCD *state);
-#define ClownCD_IsOpen(state) ClownCD_FileIsOpen(&(state)->file)
+#define ClownCD_Open(disc, file_path, callbacks) ClownCD_OpenAlreadyOpen(disc, NULL, file_path, callbacks)
+void ClownCD_OpenAlreadyOpen(ClownCD *disc, void *stream, const char *file_path, const ClownCD_FileCallbacks *callbacks);
+void ClownCD_Close(ClownCD *disc);
+#define ClownCD_IsOpen(disc) ClownCD_FileIsOpen(&(disc)->file)
 
-#define ClownCD_SeekTrackIndex(state, track, index) ClownCD_SetState(state, track, index, 0)
-#define ClownCD_SeekSector(state, sector) ClownCD_SeekAudioFrame((state), (size_t)(sector) * CLOWNCD_AUDIO_FRAMES_PER_SECTOR)
-cc_bool ClownCD_SeekAudioFrame(ClownCD *state, size_t frame);
+#define ClownCD_SeekTrackIndex(disc, track, index) ClownCD_SetState(disc, track, index, 0)
+#define ClownCD_SeekSector(disc, sector) ClownCD_SeekAudioFrame((disc), (size_t)(sector) * CLOWNCD_AUDIO_FRAMES_PER_SECTOR)
+cc_bool ClownCD_SeekAudioFrame(ClownCD *disc, size_t frame);
 
-cc_bool ClownCD_SetState(ClownCD *state, unsigned int track, unsigned int index, size_t frame);
+cc_bool ClownCD_SetState(ClownCD *disc, unsigned int track, unsigned int index, size_t frame);
 
-cc_bool ClownCD_BeginSectorStream(ClownCD* state);
-size_t ClownCD_ReadSectorStream(ClownCD* state, unsigned char *buffer, size_t total_bytes);
-cc_bool ClownCD_EndSectorStream(ClownCD* state);
-size_t ClownCD_ReadSector(ClownCD* state, unsigned char *buffer);
+cc_bool ClownCD_BeginSectorStream(ClownCD* disc);
+size_t ClownCD_ReadSectorStream(ClownCD* disc, unsigned char *buffer, size_t total_bytes);
+cc_bool ClownCD_EndSectorStream(ClownCD* disc);
+size_t ClownCD_ReadSector(ClownCD* disc, unsigned char *buffer);
 
-size_t ClownCD_ReadFrames(ClownCD *state, short *buffer, size_t total_frames);
+size_t ClownCD_ReadFrames(ClownCD *disc, short *buffer, size_t total_frames);
 
 unsigned long ClownCD_CalculateSectorCRC(const unsigned char *buffer);
 cc_bool ClownCD_ValidateSectorCRC(const unsigned char *buffer);
