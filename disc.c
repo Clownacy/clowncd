d502f0c210c4094f4c99252d6e85b6deef2abb92
Clownacy <Clownacy@users.noreply.github.com>
2026-01-31 01:16:58 +0000

    Disable CHD support in unity builds.
    I need to figure out how to handle forced include paths in unity
    builds...

--- disc.c
+++ disc.c
@@ -3,15 +3,20 @@
 #include <assert.h>
 
 #include "disc/clowncd.h"
+#ifdef CLOWNCD_CHD
 #include "disc/chd.h"
+#endif
 #include "disc/cue.h"
 #include "disc/raw.h"
 
 static ClownCD_DiscType ClownCD_GetDiscType(ClownCD_File* const file)
 {
+#ifdef CLOWNCD_CHD
 	if (ClownCD_Disc_CHDDetect(file))
 		return CLOWNCD_DISC_CHD;
-	else if (ClownCD_Disc_RawDetect(file))
+	else
+#endif
+	if (ClownCD_Disc_RawDetect(file))
 		return CLOWNCD_DISC_RAW_2352;
 	else if (ClownCD_Disc_ClownCDDetect(file))
 		return CLOWNCD_DISC_CLOWNCD;
@@ -53,10 +58,11 @@ void ClownCD_DiscOpen(ClownCD_Disc* const disc)
 		case CLOWNCD_DISC_CLOWNCD:
 			ClownCD_Disc_ClownCDOpen(disc);
 			break;
-
+#ifdef CLOWNCD_CHD
 		case CLOWNCD_DISC_CHD:
 			ClownCD_Disc_CHDOpen(disc);
 			break;
+#endif
 	}
 }
 
@@ -73,9 +79,10 @@ cc_bool ClownCD_DiscSeekTrackIndex(ClownCD_Disc* const disc, const unsigned int 
 
 		case CLOWNCD_DISC_CLOWNCD:
 			return ClownCD_Disc_ClownCDSeekTrackIndex(disc, track, index);
-
+#ifdef CLOWNCD_CHD
 		case CLOWNCD_DISC_CHD:
 			return ClownCD_Disc_CHDSeekTrackIndex(disc, track, index);
+#endif
 	}
 
 	return cc_true;
