a8c4c7f5bf4ded288b6df2299714672fbac8fb07
Clownacy <Clownacy@users.noreply.github.com>
2026-02-02 00:38:30 +0000

    Remove unused(?) integer typedefs.
    They just conflict with other dumb typedefs in libretro unity builds.

--- disc/chd/libchdr/coretypes.h
+++ disc/chd/libchdr/coretypes.h
@@ -6,20 +6,6 @@
 
 #include "../retro_inline.h"
 
-typedef uint64_t UINT64;
-#ifndef OSD_CPU_H
-typedef uint32_t UINT32;
-typedef uint16_t UINT16;
-typedef uint8_t UINT8;
-#endif
-
-typedef int64_t INT64;
-#ifndef OSD_CPU_H
-typedef int32_t INT32;
-typedef int16_t INT16;
-typedef int8_t INT8;
-#endif
-
 #ifndef ARRAY_LENGTH
 #define ARRAY_LENGTH(x) (sizeof(x)/sizeof(x[0]))
 #endif
