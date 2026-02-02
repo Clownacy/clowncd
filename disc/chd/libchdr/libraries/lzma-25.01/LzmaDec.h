022c4642a3d1da15b8c464c737c3f3b205019884
Clownacy <Clownacy@users.noreply.github.com>
2026-02-02 01:34:39 +0000

    Fix LZMA namespace.
    Derp.

--- disc/chd/libchdr/libraries/lzma-25.01/LzmaDec.h
+++ disc/chd/libchdr/libraries/lzma-25.01/LzmaDec.h
@@ -1,13 +1,13 @@
 /* Namespace some symbols to avoid linker errors in static libretro builds. */
-#define ClownCD_CHD_LzmaDec_InitDicAndState LzmaDec_InitDicAndState
-#define ClownCD_CHD_LzmaDec_Init LzmaDec_Init
-#define ClownCD_CHD_LzmaDec_DecodeToDic LzmaDec_DecodeToDic
-#define ClownCD_CHD_LzmaDec_DecodeToBuf LzmaDec_DecodeToBuf
-#define ClownCD_CHD_LzmaDec_FreeProbs LzmaDec_FreeProbs
-#define ClownCD_CHD_LzmaDec_Free LzmaDec_Free
-#define ClownCD_CHD_LzmaProps_Decode LzmaProps_Decode
-#define ClownCD_CHD_LzmaDec_AllocateProbs LzmaDec_AllocateProbs
-#define ClownCD_CHD_LzmaDec_Allocate LzmaDec_Allocate
-#define ClownCD_CHD_LzmaDecode LzmaDecode
+#define LzmaDec_InitDicAndState ClownCD_CHD_LzmaDec_InitDicAndState
+#define LzmaDec_Init ClownCD_CHD_LzmaDec_Init
+#define LzmaDec_DecodeToDic ClownCD_CHD_LzmaDec_DecodeToDic
+#define LzmaDec_DecodeToBuf ClownCD_CHD_LzmaDec_DecodeToBuf
+#define LzmaDec_FreeProbs ClownCD_CHD_LzmaDec_FreeProbs
+#define LzmaDec_Free ClownCD_CHD_LzmaDec_Free
+#define LzmaProps_Decode ClownCD_CHD_LzmaProps_Decode
+#define LzmaDec_AllocateProbs ClownCD_CHD_LzmaDec_AllocateProbs
+#define LzmaDec_Allocate ClownCD_CHD_LzmaDec_Allocate
+#define LzmaDecode ClownCD_CHD_LzmaDecode
 
 #include "real/LzmaDec.h"
