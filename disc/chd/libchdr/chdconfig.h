2c0864807101c5b2c67fee41ef9db56c7700d3be
Clownacy <Clownacy@users.noreply.github.com>
2026-02-05 12:49:55 +0000

    Disable unused libchdr features.
    ClownCD doesn't need them.

--- disc/chd/libchdr/chdconfig.h
+++ disc/chd/libchdr/chdconfig.h
@@ -4,8 +4,8 @@
 #include "../retro_inline.h"
 
 /* Configure CHDR features here */
-#define WANT_RAW_DATA_SECTOR    1
-#define WANT_SUBCODE            1
+/*#define WANT_RAW_DATA_SECTOR    1*/
+/*#define WANT_SUBCODE            1*/
 /*#define NEED_CACHE_HUNK         1*/
 /*#define VERIFY_BLOCK_CRC        1*/
 
