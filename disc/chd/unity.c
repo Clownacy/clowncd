55ad3f4a5409f6db5225f397b3903ef7fbde74c7
Clownacy <Clownacy@users.noreply.github.com>
2026-02-07 23:18:33 +0000

    Update libchdr.

--- disc/chd/unity.c
+++ disc/chd/unity.c
@@ -1,2 +1,8 @@
+/* Disable libchdr features that we do not need. */
+#define WANT_RAW_DATA_SECTOR 0
+#define WANT_SUBCODE 0
+#define VERIFY_BLOCK_CRC 0
+
 #include "libchdr/unity.c"
+
 #include "streams/chd_stream.c"
